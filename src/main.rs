10ab55876a3e096a928a0919556717765b4e9d9b
BioGustav <65023470+BioGustav@users.noreply.github.com>
2022-12-01 13:58:36 +0100

    day1 part1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use fileinput::FileInput;
+use std::io::{BufRead, BufReader};
 fn main() {
-    println!("Hello, world!");
+    day1_1();
+}
+
+fn day1_1() {
+    let filenames = vec!["./test/day1_1"];
+    let fileinput = FileInput::new(&filenames);
+    let reader = BufReader::new(fileinput);
+
+    let mut max = 0;
+    let mut sum = 0;
+    for line in reader.lines() {
+        let text = line.unwrap();
+        if text != "" {
+            let my_int: i32 = text.to_string().parse().unwrap();
+            sum += my_int;
+        } else {
+            if sum > max {
+                max = sum;
+            }
+            sum = 0;
+        }
+    }
+    if sum > max {
+        max = sum;
+    }
+    println!("{}", max);
 }
