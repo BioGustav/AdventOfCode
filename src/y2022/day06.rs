36b3f9213aac9d1a17fa52c02da8c06d9db718e5
BioGustav <65023470+BioGustav@users.noreply.github.com>
2022-12-06 21:16:32 +0100

    Alternative solution for day6

--- src/y2022/day06.rs
+++ src/y2022/day06.rs
@@ -40,17 +40,27 @@ fn calc_index(input: &str, window_length: usize) -> usize {
 
 // --------------------- ALTERNATIVE SOLUTIONS ---------------------
 
-#[aoc(day6, part1, alt)]
-fn part1_alt(input: &str) -> usize {
-    calc_index_alt(input, 4)
+#[aoc(day6, part1, alt0)]
+fn part1_alt0(input: &str) -> usize {
+    calc_index_alt0(input, 4)
 }
 
-#[aoc(day6, part2, alt)]
+#[aoc(day6, part2, alt0)]
 fn part2_alt(input: &str) -> usize {
-    calc_index_alt(input, 14)
+    calc_index_alt0(input, 14)
 }
 
-fn distinct_alt(window: &[&u8]) -> bool {
+#[aoc(day6, part1, alt1)]
+fn part1_alt1(input: &str) -> usize {
+    calc_index_alt1(input, 4)
+}
+
+#[aoc(day6, part2, alt1)]
+fn part2_alt1(input: &str) -> usize {
+    calc_index_alt1(input, 14)
+}
+
+fn distinct_alt0(window: &[&u8]) -> bool {
     for (i, a) in window.iter().enumerate() {
         for b in window[(i + 1)..].iter() {
             if a == b {
@@ -61,17 +71,32 @@ fn distinct_alt(window: &[&u8]) -> bool {
     true
 }
 
-fn calc_index_alt(input: &str, window_length: usize) -> usize {
+fn calc_index_alt0(input: &str, window_length: usize) -> usize {
     let mut input = input.as_bytes().iter();
     let mut index = window_length;
     let mut window = vec![];
     for _ in 0..window_length {
         window.push(input.next().unwrap());
     }
-    while !distinct_alt(&window) {
+    while !distinct_alt0(&window) {
         window.rotate_left(1);
         window[3] = input.next().unwrap();
         index += 1;
     }
     index
+}
+
+fn calc_index_alt1(input: &str, window_length: usize) -> usize {
+    let input = input.as_bytes();
+    for i in 0..input.len() {
+        let window = &input[i..(i + window_length)];
+        if distinct_alt1(window) {
+            return i + window_length;
+        }
+    }
+    0
+}
+
+fn distinct_alt1(window: &[u8]) -> bool {
+    !(1..window.len()).any(|i| window[i..].contains(&window[i - 1]))
 }
