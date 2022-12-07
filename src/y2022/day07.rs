3cb27efb47cfba475f252940948d55678fb1e7da
BioGustav <65023470+BioGustav@users.noreply.github.com>
2022-12-07 01:12:02 +0100

    day7 init

--- src/y2022/day07.rs
+++ src/y2022/day07.rs
@@ -1,29 +1,18 @@
-use std::num::ParseIntError;
+use std::string::ParseError;
 
 use aoc_runner_derive::{aoc, aoc_generator};
 
-#[aoc_generator(day2)]
-fn parse_input_day1(input: &str) -> Result<Vec<i32>, ParseIntError> {
-    // Ok(input
-    //     .split("\n\n")
-    //     .collect::<Vec<_>>()
-    //     .iter()
-    //     .map(|group| group.split("\n").collect::<Vec<_>>())
-    //     .map(|v| v.iter().map(|&s| s.parse::<i32>().unwrap()).sum())
-    //     .collect::<Vec<_>>())
-
-    Ok(vec![0, 0, 0])
+#[aoc_generator(day7)]
+fn parse_input_day1(input: &str) -> Result<String, ParseError> {
+    panic!()
 }
 
-#[aoc(day2, part1)]
-fn part1(calories: &[i32]) -> i32 {
-    *calories.into_iter().reduce(|a, b| a.max(b)).unwrap()
+#[aoc(day7, part1)]
+fn part1(calories: &str) -> i32 {
+    panic!()
 }
 
-#[aoc(day2, part2)]
-fn part2(calories: &[i32]) -> i32 {
-    let mut calories = calories.to_owned();
-    calories.sort_by(|a, b| b.cmp(a));
-    let top3 = &calories[..3];
-    top3.into_iter().sum()
+#[aoc(day7, part2)]
+fn part2(calories: &str) -> i32 {
+    panic!()
 }
